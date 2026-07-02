b0263b7618ca0b5dc594cba8b1a36331a9f44ee0
jonahhess <jonahhessdev@gmail.com>
2026-07-02 12:52:33 +0300

    added route to get keywords

--- routes/courses.js
+++ routes/courses.js
@@ -1,28 +1,32 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const auth = require('../middleware/auth');
+const auth = require("../middleware/auth");
 const {
-    createCourse,
-    getAllCourses,
-    getCourseById,
-    updateCourse,
-    deleteCourse
-} = require('../controllers/courseController');
-router.use(auth)
+  createCourse,
+  getAllCourses,
+  getCourseById,
+  updateCourse,
+  deleteCourse,
+  getAllKeywords,
+} = require("../controllers/courseController");
+router.use(auth);
 
 // Create a new course
-router.post('/', createCourse);
+router.post("/", createCourse);
 
 // Get all courses
-router.get('/', getAllCourses);
+router.get("/", getAllCourses);
 
 // Get a course by ID
-router.get('/:id', getCourseById);
+router.get("/:id", getCourseById);
 
 // Update a course by ID
-router.put('/:id', updateCourse);
+router.put("/:id", updateCourse);
 
 // Delete a course by ID
-router.delete('/:id', deleteCourse);
+router.delete("/:id", deleteCourse);
+
+// Get all keywords for all courses
+router.get("/keywords", getAllKeywords);
 
 module.exports = router;

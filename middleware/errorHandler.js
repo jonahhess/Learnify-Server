c17501a0294561c123fa464ca7df7f39c548556d
jonahhess <jonahhessdev@gmail.com>
2026-07-05 12:20:06 +0300

    a helper not to resend headers

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,16 +1,20 @@
 const errorHandler = (err, req, res, next) => {
+  if (res.headersSent) {
+    return next(err);
+  }
+
   let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
   let message = err.message;
 
   // Mongoose duplicate key error
   if (err.code === 11000) {
     statusCode = 400;
-    message = 'Duplicate field value entered';
+    message = "Duplicate field value entered";
   }
 
   res.status(statusCode).json({
     message: message,
-    stack: process.env.NODE_ENV === 'production' ? null : err.stack,
+    stack: process.env.NODE_ENV === "production" ? null : err.stack,
   });
 };
 
